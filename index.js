4041dd7298713523afbd605352e591ffb54f2899
Alexander Cobleigh <cblgh@cblgh.org>
2017-08-16 22:22:39 +0200

    ok added following

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require("mz/fs")
 var hyperdrive = require("hyperdrive")
 var request = require("request")
 
-// TODO: add dat://<key>
+// TODO: add support for dat://<key>
 var files = ["https://test-cblgh.hashbase.io", "/Users/cblgh/code/rotonde-merge/rotonde1.json"]
 var originPath = "./rotonde.json"
 var statePath = "./state.json"
@@ -32,6 +32,16 @@ function createStateFile() {
     })
 }
 
+/*
+ what kind of state do we save?
+ * the unixtime of the last post
+ * portal.profile as it was the last time we loaded
+     * if any attribute in portal.profile has changed (remote.current_profile.attrib != remote.previous_profile.attrib=
+       then update the corresponding attribute for origin. 
+       once all attributes have been checked: set remote.previous_profile = remote.current_profile
+    TODO: add support for portal-array
+
+*/
 function defaultState() {
     return {
         lastTimestamp: 0, 
@@ -41,7 +51,8 @@ function defaultState() {
             color: "",
             position: "",
             avatar: ""
-        }
+        },
+        portal: []
         // TODO: add portals
     }
 }
@@ -98,17 +109,6 @@ path.stat(statePath).catch(function(err) {
     })
 })
 
-/*
- what kind of state do we save?
- * the unixtime of the last post
- * portal.profile as it was the last time we loaded
-     * if any attribute in portal.profile has changed (remote.current_profile.attrib != remote.previous_profile.attrib=
-       then update the corresponding attribute for origin. 
-       once all attributes have been checked: set remote.previous_profile = remote.current_profile
-
-*/
-
-// CALLBACK THAT HANDLES A JSON FILE, GETTING ITS ROTONDE CONTENTS
 function processJSON(key, contents) {
     return new Promise(function(resolve, reject) {
         if (!savedState[key]) { 
@@ -131,7 +131,7 @@ function processJSON(key, contents) {
         // save the current profile
         state.profile = current_profile
 
-        // get the new posts
+        // get any new posts
         var posts = getNewPosts(state.lastTimestamp, contents.feed)
         posts.map(function(post) {
             var dupe = false
@@ -149,13 +149,67 @@ function processJSON(key, contents) {
             }
         })
 
+        // update timestamp of newest post
         if (posts.length > 0) {
             state.lastTimestamp = posts[posts.length - 1].time 
         }
 
-        // save state to disk
+        var portals = updatePortals(state, contents.portal)
+        // remove the unfollowed portals
+        portals.removed.forEach(function(portal) {
+            var index = origin.portal.indexOf(portal)
+            if (index >= 0) {
+                console.log("unfollowing", portal)
+                origin.portal.splice(index, 1)
+            }
+        })
+        // follow the added portals
+        portals.added.forEach(function(portal) {
+            if (origin.portal.indexOf(portal) < 0) {
+                console.log("following", portal)
+                origin.portal.push(portal)
+            }
+        })
+
         savedState[key] = state
-        saveJSON(savedState, statePath).catch(function(err) {
+        // persist state to disk
+        save().then(resolve).catch(reject)
+    })
+}
+
+// TODO: rewrite this part with functional javascript i.e. filter or something more apt
+function updatePortals(state, currentPortals) {
+    var portals = {removed: [], added: []}
+    var removed = []
+    state.portal.map(function(portal) {
+        // portal has been removed if it was in state.portal yet isn't in currentPortals
+        var index = currentPortals.indexOf(portal)
+        if (index < 0) {
+            removed.push(state.portal.indexOf(portal))
+            portals.removed.push(portal)
+        }
+    })
+    removed.reverse()
+    // remove from the state in reverse order, so the indexes aren't affected
+    for (var index in removed) {
+        state.portal.splice(index, 1)
+    }
+    // portal has been added if it isn't in state.portal, but is in currentPortals
+    currentPortals.map(function(portal) {
+        // portal has been removed if it was in state.portal yet isn't in currentPortals
+        var index = state.portal.indexOf(portal)
+        if (index < 0) {
+            portals.added.push(portal)
+            state.portal.push(portal)
+        }
+    })
+    return portals
+}
+
+function save() {
+    return new Promise(function(resolve, reject) {
+        saveJSON(savedState, statePath)
+        .catch(function(err) {
             console.error("err when saving state in processJSON")
             reject()
         })
@@ -166,9 +220,7 @@ function processJSON(key, contents) {
             console.error("err when saving origin in processJSON")
             reject()
         })
-        .then(function() {
-            resolve()
-        })
+        .then(resolve)
     })
 }
 
