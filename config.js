a200764d603b0e18ec65fcf245ec78b6bab9ff6e
Alexander Cobleigh <angellus.falsus@gmail.com>
2017-08-17 13:09:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 module.exports = {
-    // absolute path to the single file you want to serve, the merged rotonde.json
+    // absolute path to the merged file you want to serve e.g. /http/rotonde/public/rotonde.json
     originPath: "", 
-    // absolute path to your state
+    
+    // absolute path to your state e.g. /path/to/rotonde-merge/state.json
     statePath: "", 
+    
     // rotonde files you want to merge into one
     // supported types are:
     // * local file system paths /path/to/rotonde.json
     // * http(s) urls e.g. http://rotonde.json.org, or http://rotonde.org/rotonde.json
     //   note: it's important to include http:// or https:// for URLs
+    // e.g. files: ["http://rotonde.xxiivv.com", "/home/xxiivv/rotonde/rotonde.json"]
     files: []
 }
